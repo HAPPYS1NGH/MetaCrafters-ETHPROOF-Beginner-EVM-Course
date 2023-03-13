78620b79392b20e098638c95ee29a5fbf5357059
Harpreet Singh <92804770+HAPPYS1NGH@users.noreply.github.com>
2023-03-13 11:48:16 +0530

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,11 +1,10 @@
-// SPDX-License-Identifier: GPL-3.0
+// SPDX-License-Identifier: MIT
 
-pragma solidity >=0.8.2 <0.9.0;
+pragma solidity ^0.8.2;
 
 /**
  * @title Storage
  * @dev Store & retrieve value in a variable
- * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
  */
 contract Storage {
 
@@ -15,8 +14,8 @@ contract Storage {
      * @dev Store value in variable
      * @param num value to store
      */
-    function store(uint256 num) public {
-        number = num;
+    function store(uint256 _num) public {
+        number = _num;
     }
 
     /**
